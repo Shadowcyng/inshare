ab2174c3310a6f97cc282d8fba844607f01810b1
Satyam Singhania <45529137+Shadow-Cyng@users.noreply.github.com>
2020-09-03 22:59:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000; 
 const connectDB = require('./config/db');
+const cors = require('cors')
 const path =  require('path')
 connectDB();
 
 //Template engine
 app.set('views',path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
-//Router import
+//Router import 
 app.get('/',()=>console.log('server is up'))
 
 app.use(express.static('public'));
 app.use(express.json())
 
+//Cors
+const corsOption = {
+    origin : process.env.ALLOWED_CLIENTS.split(','),
+    //[ 'http://localhost:5000', 'http://localhost:3000' ]
+}
+app.use(cors(corsOption));
 //Routes
 app.use('/api/files',require('./routes/files'));
 app.use('/files', require('./routes/show'));
